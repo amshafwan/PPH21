4b5f03d1aad63241682308a94b11f1547d2a04e8
muhammad shafwan <auan.30108@gmail.com>
2023-02-08 08:35:49 +0700

    pph21

---
+++ App/Calculate/FourthRuleCalculator.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Calculate;
+
+class FourthRuleCalculator extends AbstractCalculator
+{
+public function maxPkp(): float
+{
+return 10000000000000000;//bikin sebesar mungkin sampai impossible ada orang setahun dapat segitu
+}
+public function minPkp(): float
+{
+return 500000000;
+}
+public function taxPercentage(): float
+{
+return 0.3;
+}
+}
---
+++ App/init.php
@@ -0,0 +1,11 @@
+<?php
+
+spl_autoload_register(function ($class)
+{
+    //$class = App\Produk\User
+    //$class = [App, Produk, User]
+    $class = explode('\\', $class);
+    $class = end($class);
+
+    require_once __DIR__ . '/Calculate/' . $class . '.php';
+});
